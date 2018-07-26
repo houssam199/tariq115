4218a7f77b318c501f557ba14a303d97814cc27d
houssam199 <40376744+houssam199@users.noreply.github.com>
2018-07-26 11:33:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ client.on('message', async msg => { // eslint-disable-line
 	let command = msg.content.toLowerCase().split(" ")[0];
 	command = command.slice(PREFIX.length)
 
-	if (command === `play`) {
+	if (command === `Tplay`) {
 		const voiceChannel = msg.member.voiceChannel;
 		if (!voiceChannel) return msg.channel.send('أنا آسف ولكن عليك أن تكون في قناة صوتية لتشغيل الموسيقى!');
 		const permissions = voiceChannel.permissionsFor(msg.client.user);
